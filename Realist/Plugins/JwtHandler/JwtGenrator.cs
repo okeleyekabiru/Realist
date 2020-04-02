a6c1bebd916700fca33f396043cf02ee38a358cd
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-02 22:09:13 +0100

    Add jwttoken generator

--- Realist/Plugins/JwtHandler/JwtGenrator.cs
+++ Realist/Plugins/JwtHandler/JwtGenrator.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
+using Microsoft.IdentityModel.Tokens;
 using Realist.Data.Model;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 namespace Plugins.JwtHandler
 {
@@ -9,7 +13,30 @@ namespace Plugins.JwtHandler
     {
         public JwtModel CreateToken(User user)
         {
-            throw new NotImplementedException();
+            var claim = new List<Claim>
+            {
+
+                new Claim(JwtRegisteredClaimNames.NameId, user.Id)
+
+
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("my super secret key"));
+            var credentials =  new SigningCredentials(key,SecurityAlgorithms.HmacSha256Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject= new ClaimsIdentity(claims:claim),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = credentials
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+           var token =  tokenHandler.CreateToken(tokenDescriptor);
+          var returnToken = tokenHandler.WriteToken(token);
+
+          return new JwtModel
+          {
+              ExpiryDate = token.ValidTo,
+              Token = returnToken
+          };
         }
     }
 }
