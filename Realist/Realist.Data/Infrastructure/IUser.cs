dce3f1d9eabbd50f99f18987164676d1e5756e12
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-02 23:46:19 +0100

    Add jwt interface

---
+++ Realist/Realist.Data/Infrastructure/IUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Plugins.JwtHandler;
+using Realist.Data.Model;
+
+namespace Realist.Data.Infrastructure
+{
+  public  interface IUser
+  {
+      User GetCurrentUser(string userId);
+      Task<JwtModel> RegisterUser(User user);
+  }
+}
