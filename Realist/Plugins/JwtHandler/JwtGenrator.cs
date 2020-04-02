3af7150bb39fd77d67451445c51a10d87248416c
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-02 17:20:50 +0100

    Add jwthandler to plugin

---
+++ Realist/Plugins/JwtHandler/JwtGenrator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Realist.Data.Model;
+
+namespace Plugins.JwtHandler
+{
+  public  class JwtGenrator:IJwtSecurity
+    {
+        public JwtModel CreateToken(User user)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
