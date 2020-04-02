85aa1bf45f2c0ef57c76bddf48433813fd6300f7
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-02 17:11:21 +0100

    Add db context set up

---
+++ Realist/Realist.Data/Services/DataContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Realist.Data.Model;
+
+namespace Realist.Data.Services
+{
+  public  class DataContext:IdentityDbContext<User>
+    {
+        public virtual DbSet<Post> Posts { get; set; }
+        public virtual DbSet<Comment> Comments { get; set; }
+        public  virtual  DbSet<Reply> Replies { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        base.OnModelCreating(builder);
+        builder.Entity<User>(opt =>
+        {
+            opt.HasKey(o => o.Id);
+            opt.HasMany(o => o.Photo)
+                .WithOne().IsRequired()
+                .HasForeignKey(r => r.UserId);
+            opt.HasMany(o => o.Videos)
+                .WithOne().IsRequired()
+                .HasForeignKey(r => r.UserId)
+                ;
+
+        });
+        builder.Entity<Post>(opt =>
+        {
+            opt.HasKey(o => o.Id);
+            opt.HasMany(o => o.Comments).WithOne();
+            opt.HasOne(o => o.User).WithMany(o => o.Posts).IsRequired()
+                .HasForeignKey(r => r.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+        });
+        builder.Entity<Comment>(opt =>
+        {
+            opt.HasMany(o => o.Replies)
+                .WithOne().IsRequired()
+                .HasForeignKey(r => r.CommentId)
+                .OnDelete(DeleteBehavior.Cascade);
+        });
+        builder.Entity<Reply>(opt =>
+        {
+            opt.HasOne(o => o.Comment)
+                .WithMany(o => o.Replies)
+                .HasForeignKey(r => r.CommentId);
+        });
+
+        }
+    }
+}
