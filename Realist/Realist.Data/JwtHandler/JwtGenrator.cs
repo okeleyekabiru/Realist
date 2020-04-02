311936cbadefe0d41ba3fbcfc5aeeb3e87a2c78a
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-02 23:47:11 +0100

    add jwthandler

---
+++ Realist/Realist.Data/JwtHandler/JwtGenrator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Realist.Data.Model;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
+
+namespace Plugins.JwtHandler
+{
+  public  class JwtGenrator:IJwtSecurity
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtGenrator(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public JwtModel CreateToken(User user)
+        {
+            var claim = new List<Claim>
+            {
+
+                new Claim(JwtRegisteredClaimNames.NameId, user.Id)
+
+
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("jwtHandler").Value));
+            var credentials =  new SigningCredentials(key,SecurityAlgorithms.HmacSha256Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject= new ClaimsIdentity(claims:claim),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = credentials
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+           var token =  tokenHandler.CreateToken(tokenDescriptor);
+          var returnToken = tokenHandler.WriteToken(token);
+
+          return new JwtModel
+          {
+              ExpiryDate = token.ValidTo,
+              Token = returnToken
+          };
+        }
+    }
+}
---
+++ Realist/Realist.Data/JwtHandler/JwtModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugins.JwtHandler
+{
+   public class JwtModel
+   {
+      public string Token { get; set; }
+      public DateTime ExpiryDate { get; set; }
+   }
+}
