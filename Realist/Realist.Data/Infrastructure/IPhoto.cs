03ae3fb5ac2cd2419bf7bae3fe0432c27dd16c4f
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-03 17:32:15 +0100

    Add Photo contxt interface

---
+++ Realist/Realist.Data/Infrastructure/IPhoto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Realist.Data.Model;
+
+namespace Realist.Data.Infrastructure
+{
+  public  interface  IPhoto
+  {
+      Task UploadImageDb(Photo photo);
+      void DeleteImageFromDb(Photo photo);
+      Task<bool> SaveChanges();
+      Task<bool> FindUserImage(string userId);
+  }
+}
