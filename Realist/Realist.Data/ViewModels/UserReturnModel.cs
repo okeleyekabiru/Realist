722a5a931c5c0f5b0caa087d95bb143b154a5535
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-06 13:48:35 +0100

    Add model to return token and expiry date

---
+++ Realist/Realist.Data/ViewModels/UserReturnModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Realist.Data.ViewModels
+{
+  public  class UserReturnModel
+    {
+        public string Token { get; set; }
+        public DateTime ExpiryDate { get; set; }
+    }
+}
