a7cfcc429bd7ddccb3f00938be6f11431f4d6224
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-06 13:51:45 +0100

    Add read token method and email token

--- Realist/Realist.Data/JwtHandler/JwtGenrator.cs
+++ Realist/Realist.Data/JwtHandler/JwtGenrator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using Microsoft.Extensions.Configuration;
@@ -44,6 +45,37 @@ namespace Plugins.JwtHandler
               ExpiryDate = token.ValidTo,
               Token = returnToken
           };
+        }
+
+        public string ReadToken(string token)
+        {
+           
+            var handler = new JwtSecurityTokenHandler();
+            var decodeToken = handler.ReadJwtToken(token);
+         return decodeToken.Claims?.First().Value;
+         
+        }
+
+        public string CreateTokenForEmail(User user)
+        {
+            var claim = new List<Claim>
+            {
+
+                new Claim(JwtRegisteredClaimNames.NameId, user.Id)
+
+
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("jwtHandler").Value));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims: claim),
+                Expires = DateTime.Now.AddMinutes(10),
+                SigningCredentials = credentials
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
     }
 }
