db29bf69ad45b6d90da6d8540645706bb1d661ad
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-06 13:53:28 +0100

    Add code property

--- Realist/Realist.Data/ViewModels/UserModel.cs
+++ Realist/Realist.Data/ViewModels/UserModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -8,8 +9,12 @@ namespace Realist.Api.ViewModels
 {
     public class UserModel
     {
+        [Required]
         public string Email { get; set; }
+        [Required]
         public string Password { get; set; }
+
+        public string UserName { get; set; }
         public  IFormFile Photo { get; set; }
     }
 }
