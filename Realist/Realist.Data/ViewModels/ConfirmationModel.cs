db2c28bc9d5d953dacc60a9d4492542241e93ab3
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-06 13:48:03 +0100

    Add mail confimation model

---
+++ Realist/Realist.Data/ViewModels/ConfirmationModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Realist.Data.ViewModels
+{
+  public  class ConfirmationModel
+    {
+        public string Token { get; set; }
+        public string Id { get; set; }
+    }
+}
