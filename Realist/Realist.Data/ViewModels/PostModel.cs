133dd9931a97474174af46b7fdb838eb428f5808
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-08 11:52:21 +0100

    Add postmodel

---
+++ Realist/Realist.Data/ViewModels/PostModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Realist.Data.Model;
+
+namespace Realist.Data.ViewModels
+{
+  public  class PostModel
+    {
+        public string Body { get; set; }
+        public Photo Photo { get; set; }
+        public Videos Video { get; set; }
+        public Category? Category { get; set; }
+        public News? News { get; set; }
+        public Articles? Articles { get; set; }
+    }
+}
