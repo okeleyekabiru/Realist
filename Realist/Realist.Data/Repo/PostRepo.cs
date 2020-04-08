d8eaed6aa28e479c3f0f0b6464946174368adc39
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-08 17:47:06 +0100

    add get all

--- Realist/Realist.Data/Repo/PostRepo.cs
+++ Realist/Realist.Data/Repo/PostRepo.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Realist.Data.Infrastructure;
 using Realist.Data.Model;
 using Realist.Data.Services;
@@ -21,6 +22,13 @@ namespace Realist.Data.Repo
             await _context.Posts.AddAsync(post);
 
            
+        }
+
+        public async Task<IEnumerable<Post>> GetAll()
+        {
+            return await _context.Posts.Include(r => r.Photos).Include(r => r.Videos).Include(r => r.Comments)
+                .ThenInclude(r => r.Replies).Include(r => r.User).ToListAsync();
+
         }
 
         public async Task<bool> SaveChanges()
