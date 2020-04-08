1ccb1ad63295ba6e0a7bb0be3cd120591e9678f6
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-08 20:05:29 +0100

    Add get a post

---
+++ Realist/Realist.Data/ViewModels/GetPostModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.AccessControl;
+using System.Text;
+
+namespace Realist.Data.ViewModels
+{
+   public class GetPostModel
+    {
+        public string Id { get; set; }
+    }
+}
