89b9462f67bf34b1d9981f81fa852ef1d07c8c0e
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-08 12:06:44 +0100

    Add neccesary dependency

--- Realist/Realist.Api/Controllers/PostController.cs
+++ Realist/Realist.Api/Controllers/PostController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Plugins.Mail;
+using Plugins.Youtube;
+using Realist.Data.Infrastructure;
 
 namespace Realist.Api.Controllers
 {
@@ -11,6 +15,21 @@ namespace Realist.Api.Controllers
     [ApiController]
     public class PostController : ControllerBase
     {
+        private readonly IPost _postContext;
+        private readonly IMailService _mailService;
+        private readonly ILogger<PostController> _logger;
+        private readonly IUser _userContext;
+        private readonly IPhoto _photoUpload;
+        private readonly IYoutube _youtubeuploader;
 
+        public PostController(IPost postContext,IMailService mailService,ILogger<PostController> logger,IUser userContext,IPhoto photoUpload,IYoutube youtubeuploader)
+        {
+            _postContext = postContext;
+            _mailService = mailService;
+            _logger = logger;
+            _userContext = userContext;
+            _photoUpload = photoUpload;
+            _youtubeuploader = youtubeuploader;
+        }
     }
 }
