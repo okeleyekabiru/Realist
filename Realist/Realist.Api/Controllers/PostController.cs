f89ec27da90e32874fa151525f33966f454b1c59
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-08 11:53:16 +0100

    Add post controller

---
+++ Realist/Realist.Api/Controllers/PostController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Realist.Api.Controllers
+{
+    [Route("api/post")]
+    [ApiController]
+    public class PostController : ControllerBase
+    {
+
+    }
+}
