22fde0d4360e399450184a6d2ca2687db3823360
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-08 12:07:14 +0100

    Add post method

--- Realist/Realist.Data/Infrastructure/IPost.cs
+++ Realist/Realist.Data/Infrastructure/IPost.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Realist.Data.Model;
 
 namespace Realist.Data.Infrastructure
 {
     public interface IPost
     {
-        Task<bool> Post();
+        Task<bool> Post(Post post);
+        Task<bool> SaveChanges();
     }
 }
