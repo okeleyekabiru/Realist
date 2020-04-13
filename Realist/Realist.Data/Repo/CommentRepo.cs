3aadde89c498f7f1b2e3d9b663a495cccd53ba16
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-13 15:08:52 +0100

    Add update method

--- Realist/Realist.Data/Repo/CommentRepo.cs
+++ Realist/Realist.Data/Repo/CommentRepo.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Realist.Data.Infrastructure;
+using Realist.Data.ViewModels;
 using Realist.Data.Model;
 using Realist.Data.Services;
 
@@ -25,12 +26,30 @@ namespace Realist.Data.Repo
 
         }
 
+        public async Task<Comment> GetComment(string commentId)
+        {
+            return await _context.Comments.Where(e =>  e.Id.Equals(Guid.Parse(commentId))).FirstOrDefaultAsync();
+        }
+
         public async Task<bool> SaveChanges()
         {
             return await _context.SaveChangesAsync() > 0;
         }
-
-        public async Task<IEnumerable<Comment>> GetAllPostComment(string postId)
+        public async Task<ReturnResult> Update(Comment comment)
+        {
+            _context.Update(comment);
+            if (await   SaveChanges())
+            {
+                
+                return new ReturnResult{
+                Succeeded = true
+                };
+            }
+            return new ReturnResult{
+        Error = "An error occured while updating database"
+            };
+        }
+         public async Task<IEnumerable<Comment>> GetAllPostComment(string postId)
         {
             return await _context.Comments.Include(r => r.Replies).Where(r => r.PostId.Equals(Guid.Parse(postId)))
                 .ToListAsync();
