d0a1e85fa9acf7ee1d665392b3aa07d6ebb711aa
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-13 15:49:19 +0100

    Add post method for reply

---
+++ Realist/Realist.Data/Infrastructure/IReply.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Realist.Data.Model;
+using Realist.Data.ViewModels;
+
+namespace Realist.Data.Infrastructure
+{
+   public interface IReply
+   {
+       Task<ReturnResult> Add(Reply reply);
+       Task<bool> SaveChanges();
+   }
+}
