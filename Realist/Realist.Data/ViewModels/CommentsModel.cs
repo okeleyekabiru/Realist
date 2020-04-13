0e1dbdf56df5fea0d848ba6ee10e4be657435eb0
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-13 15:09:24 +0100

    add id to comment model

--- Realist/Realist.Data/ViewModels/CommentsModel.cs
+++ Realist/Realist.Data/ViewModels/CommentsModel.cs
@@ -7,9 +7,13 @@ namespace Realist.Data.ViewModels
 {
   public  class CommentsModel
     {
+        
+        [Required]
         public string Body { get; set; }
         [Required]
         public string PostId { get; set; }
+
+        public string CommentId { get; set; }
        
     }
 }
