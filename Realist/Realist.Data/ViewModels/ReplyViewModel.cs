470cdbf0e1aa9ec93c1beb2af67b39881f248b47
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-14 13:11:32 +0100

    Add Get reply

---
+++ Realist/Realist.Data/ViewModels/ReplyViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Realist.Data.Model;
+
+namespace Realist.Data.ViewModels
+{
+   public class ReplyViewModel
+    {
+        public Guid Id { get; set; }
+        public string Body { get; set; }
+        public ICollection<ReplyViewModel> Replies { get; set; }
+    }
+}
