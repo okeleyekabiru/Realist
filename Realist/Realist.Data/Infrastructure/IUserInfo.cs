a9c7d97ca5876b82c231b4009aab1a4f6e713c09
Kabiru Okeleye <kabiotobiano@gmail.com>
2020-04-15 14:08:12 +0100

    Add IUserinfo

---
+++ Realist/Realist.Data/Infrastructure/IUserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Realist.Data.Model;
+using Realist.Data.ViewModels;
+
+namespace Realist.Data.Infrastructure
+{
+   public  interface IUserInfo
+   {
+       Task<ReturnResult> Add(UserInfo userInfo);
+   }
+}
